02baffa3b34b2f6ecbb4e46fdfd3d05cb8692c34
Maik Sebastian Proba <maik.proba@hs-osnabrueck.de>
2021-03-12 15:12:56 +0100

    first commit

---
+++ js/mp-script.js
@@ -0,0 +1,4 @@
+console.log("script eingebunden!");
+
+var copyright = document.getElementById("copyright");
+copyright.innerText = "© " + new Date().getFullYear() + " mpcoding   -   made with ❤ by Maik Proba";
