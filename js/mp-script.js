d3ba618b1ba4eee07af91c77fde9963602967bdd
Maik Sebastian Proba <maik.proba@hs-osnabrueck.de>
2021-03-14 19:38:30 +0100

    scroll animation

--- js/mp-script.js
+++ js/mp-script.js
@@ -2,3 +2,36 @@ console.log("script eingebunden!");
 
 var copyright = document.getElementById("copyright");
 copyright.innerText = "© " + new Date().getFullYear() + " mpcoding   -   made with ❤ by Maik Proba";
+
+/*On Scroll Effect*/
+const allContainers = document.querySelectorAll("#mpEffect");
+console.log(allContainers);
+
+
+//Beim Scrollen wird die Funktion ausgeführt.
+window.onscroll = function mpFadeIn() {
+
+    allContainers.forEach(container => {
+        const containerTop = container.getBoundingClientRect().top;
+        //const containerBottom = containerTop.bottom;
+        var viewPosition = window.innerHeight / 1.3; 
+
+        console.log("containerTop: " + containerTop + " viewPosition: " + viewPosition);
+
+        //if (window.scrollY >= containerBottom) {
+        if( containerTop < viewPosition ){
+            console.log("!!");
+            container.style.opacity = "1";
+            container.style.transform = "translateX(0)";
+            container.style.transition = ".5s ease-in-out";
+        } 
+        
+        else {
+            container.style.opacity = "0";
+            container.style.transform = "translateY(50px)";
+            container.style.transition = ".5s ease-in-out";
+        }
+
+
+    });
+}
