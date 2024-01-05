964e89caab36b9173d64d26386b3aea25680f534
Maik <maik.proba@gmail.com>
2024-01-05 19:20:56 +0100

    update profile entfernt + xing link

--- js/mp-script.js
+++ js/mp-script.js
@@ -1,26 +1,24 @@
-const copyright = document.getElementById("copyright");
-copyright.innerText = "© " + new Date().getFullYear() + " mpcoding   -   👨‍💻 by Maik Proba";
+const copyright = document.getElementById('copyright');
+copyright.innerText =
+    '© ' + new Date().getFullYear() + ' mpcoding   -   👨‍💻 by Maik';
 
 /*On Scroll Effect*/
-const allContainers = document.querySelectorAll("#mpEffect");
-
+const allContainers = document.querySelectorAll('#mpEffect');
 
 //Beim Scrollen wird die Funktion ausgeführt.
 window.onscroll = function mpFadeIn() {
-    allContainers.forEach(container => {
+    allContainers.forEach((container) => {
         const containerTop = container.getBoundingClientRect().top;
-        var viewPosition = window.innerHeight / 1.3; 
+        var viewPosition = window.innerHeight / 1.3;
 
-        if( containerTop < viewPosition ){
-            container.style.opacity = "1";
-            container.style.transform = "translateX(0)";
-            container.style.transition = ".5s ease-in-out";
-        } 
-        
-        else {
-            container.style.opacity = "0";
-            container.style.transform = "translateY(50px)";
-            container.style.transition = ".5s ease-in-out";
+        if (containerTop < viewPosition) {
+            container.style.opacity = '1';
+            container.style.transform = 'translateX(0)';
+            container.style.transition = '.5s ease-in-out';
+        } else {
+            container.style.opacity = '0';
+            container.style.transform = 'translateY(50px)';
+            container.style.transition = '.5s ease-in-out';
         }
     });
-}
+};
